d53be89554b3b6127736615e53b6d523798351d0
G0dObject <vladimirgjob@gmail.com>
2023-03-01 17:55:07 +0300

    init

---
+++ src/redux/pizza/asyncActions.ts
@@ -0,0 +1,27 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import identity from 'lodash/identity';
+import pickBy from 'lodash/pickBy';
+import { Pizza, SearchPizzaParams } from './types';
+
+export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
+  'pizza/fetchPizzasStatus',
+  async (params) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    console.log(params);
+    const { data } = await axios.get<Pizza[]>(process.env.REACT_APP_BASE_API_URL+"items", {
+      params: pickBy(
+        {
+          page: currentPage,
+          limit: 4,
+          category,
+          sortBy,
+          order,
+          search,
+        },
+        identity,
+      ),
+    });
+    return data;
+  },
+);
